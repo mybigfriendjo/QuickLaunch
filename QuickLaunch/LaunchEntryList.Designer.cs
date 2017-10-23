1c02f8cfb79331fdb3b359e6cfd8d460dbdf3ffd
mybigfriendjo <mybigfriendjo@gmail.com>
2017-10-23 19:02:02 +0200

    added imageLists and byte comparison to not save an image twice

--- QuickLaunch/LaunchEntryList.Designer.cs
+++ QuickLaunch/LaunchEntryList.Designer.cs
@@ -24,29 +24,29 @@
         /// </summary>
         private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LaunchEntryList));
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutMain = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // tableLayoutMain
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(588, 410);
-            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutMain.ColumnCount = 2;
+            this.tableLayoutMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutMain.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutMain.Name = "tableLayoutMain";
+            this.tableLayoutMain.RowCount = 2;
+            this.tableLayoutMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutMain.Size = new System.Drawing.Size(588, 410);
+            this.tableLayoutMain.TabIndex = 0;
             // 
             // LaunchEntryList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(588, 410);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tableLayoutMain);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "LaunchEntryList";
             this.Text = "LaunchEntryList";
@@ -56,6 +56,6 @@
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutMain;
     }
 }
--- QuickLaunch/storage/LaunchRegistry.cs
+++ QuickLaunch/storage/LaunchRegistry.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
 using QuickLaunch.helper;
 
 namespace QuickLaunch.storage {
     public class LaunchRegistry {
+        // ReSharper disable once InconsistentNaming
         public static LaunchRegistry INSTANCE = new LaunchRegistry();
+        private static readonly ByteComparer byteComparer = new ByteComparer();
 
         private List<LaunchEntry> entries = new List<LaunchEntry>();
+        private Dictionary<int, byte[]> images = new Dictionary<int, byte[]>();
 
         private LaunchRegistry() { }
 
@@ -30,11 +36,56 @@ namespace QuickLaunch.storage {
             return temp;
         }
 
+        public Image GetIconImage(int posInImageList) {
+            using (MemoryStream ms = new MemoryStream(images[posInImageList])) {
+                return Image.FromStream(ms);
+            }
+        }
+
+        public int AddIconImage(Image image) {
+            byte[] temp;
+            using (MemoryStream ms = new MemoryStream()) {
+                image.Save(ms, image.RawFormat);
+                temp = ms.ToArray();
+            }
+
+            foreach (KeyValuePair<int, byte[]> current in images) {
+                if (byteComparer.Equals(temp, current.Value)) {
+                    return current.Key;
+                }
+            }
+
+            for (int i = 0; i < int.MaxValue; i++) {
+                if (images.ContainsKey(i)) {
+                    continue;
+                }
+                images.Add(i, temp);
+                return i;
+            }
+            throw new IndexOutOfRangeException("Too many Images in ImageList");
+        }
+
         public static string ToJson() {
             JsonSerializerSettings settings = new JsonSerializerSettings() {
                 ContractResolver = new PrivateMemberContractResolver()
             };
             return JsonConvert.SerializeObject(INSTANCE, settings);
+        }
+
+        private class ByteComparer : IEqualityComparer<byte[]> {
+            public ByteComparer() {
+            }
+
+            public bool Equals(byte[] x, byte[] y) {
+                if (x == null || y == null) {
+                    return x == y;
+                }
+                return x.SequenceEqual(y);
+            }
+
+            public int GetHashCode(byte[] x) {
+                return x.Sum(y => y);
+            }
         }
     }
 }
